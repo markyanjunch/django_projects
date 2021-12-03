8a8fbbf85faab7d8615cc9d60cf049241d3b1d0e
Yanjun Chen <markchen_yyjs@163.com>
2021-12-03 00:11:24 +0000

    Fixed ads model and ad_detail.html

--- mysite/ads/models.py
+++ mysite/ads/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 from django.conf import settings
 
+from taggit.managers import TaggableManager
+
 class Ad(models.Model) :
     title = models.CharField(
             max_length=200,
