24b6630b060176d79623dc218a873a9b8d00d08f
Yanjun Chen <51148553+markyanjunch@users.noreply.github.com>
2021-12-09 23:55:58 -0500

    Update views.py

--- mysite/solo/views.py
+++ mysite/solo/views.py
@@ -5,15 +5,15 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class MainView(LoginRequiredMixin, View):
     def get(self, request):
-        result = request.session.get('result', None)
+        result = request.session.get('result', False)
         if result: del(request.session['result'])
         tc = 4163
         ctx = {'TimeCode': tc, 'result': result}
         return render(request, 'solo/main.html', ctx)
         
     def post(self, request):
-        Field1 = request.POST.get('field1','')
-        Field2 = request.POST.get('field2','')
+        Field1 = request.POST.get('field1')
+        Field2 = request.POST.get('field2')
         Field1 = Field1.strip()
         Field2 = Field2.strip()
         result = Field1 + " " + Field2
